38d488dd57824dbecaea6cd775c630a2c49ba661
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-12-24 15:56:57 +0100

    1 and 2

---
+++ src/Components/SimpleCounter/index.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const SimpleCounter = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>{value}</p>
+      <button onClick={() => setValue(value + 1)}>increase</button>
+      <button onClick={() => setValue(0)}>reset</button>
+      <button
+        onClick={() => {
+          setValue(value - 1);
+        }}
+      >
+        decrease
+      </button>
+    </div>
+  );
+};
+
+export default SimpleCounter;
---
+++ src/Components/TempControlApp/index.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const TempControlApp = () => {
+  const [temp, setTemp] = useState(10);
+  const [tempColor, setTempColor] = useState();
+
+  return <div></div>;
+};
+
+export default TempControlApp;
