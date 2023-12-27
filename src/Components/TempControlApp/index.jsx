9ba5e1f4201095f53dd3c8a650adf86138167976
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-12-27 18:15:03 +0100

    temp control app

--- src/Components/TempControlApp/index.jsx
+++ src/Components/TempControlApp/index.jsx
@@ -1,10 +1,52 @@
 import { useState } from "react";
+import "./style.css";
 
 const TempControlApp = () => {
   const [temp, setTemp] = useState(10);
-  const [tempColor, setTempColor] = useState();
+  const [tempColor, setTempColor] = useState("tempCold");
 
-  return <div></div>;
+  const increaseTemp = () => {
+    if (temp === 30) return;
+    const newTemp = temp + 1;
+
+    if (newTemp >= 15) setTempColor("tempHot");
+
+    setTemp(newTemp);
+  };
+
+  const decreaseTemp = () => {
+    if (temp === 0) return;
+
+    const newTemp = temp - 1;
+
+    if (newTemp < 15) {
+      setTempColor("tempCold");
+    }
+
+    setTemp(newTemp);
+  };
+
+  const resetTemp = () => {
+    setTemp(10);
+    setTempColor("tempCold");
+  };
+
+  return (
+    <div className="frameDiv">
+      <p className={`temperature ${tempColor}`}>{temp}℃</p>
+      <div className="btnBox">
+        <button className="btn" onClick={decreaseTemp}>
+          -
+        </button>
+        <button className="resetBtn" onClick={resetTemp}>
+          Reset
+        </button>
+        <button className="btn" onClick={increaseTemp}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TempControlApp;
