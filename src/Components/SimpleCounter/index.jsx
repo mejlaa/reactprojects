83488ba48f62bfd5c04f435069e1130e826a0658
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2024-07-07 16:41:18 +0200

    povratak

--- src/Components/SimpleCounter/index.jsx
+++ src/Components/SimpleCounter/index.jsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 const SimpleCounter = () => {
   const [value, setValue] = useState(0);
-
   return (
     <div>
       <p>{value}</p>
-      <button onClick={() => setValue(value + 1)}>increase</button>
-      <button onClick={() => setValue(0)}>reset</button>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        increase
+      </button>
+      <button
+        onClick={() => {
+          setValue(0);
+        }}
+      >
+        reset
+      </button>
       <button
         onClick={() => {
           setValue(value - 1);
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SearchFilter from "./Components/SearchFilter/index.jsx";
+import SimpleCounter from "./Components/SimpleCounter/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SearchFilter />
+    <SimpleCounter />
   </React.StrictMode>
 );
